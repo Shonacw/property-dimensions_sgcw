9774f51f50fb59f68345811560b2623ee49d76a5
Anatoly Sukhanov <anatoly.sukhanov@gmail.com>
2020-06-24 19:00:23 +0300

    calculate height

--- osm.py
+++ osm.py
@@ -2,10 +2,13 @@ import sys
 import overpass
 from shapely.geometry import Polygon, Point
 from shapely.ops import transform
+from rasterstats import zonal_stats
 import pyproj
 
+raster = r"data/DTM-DSM.tif"
+
 if len(sys.argv) != 3:
-    print('osm.py -i lat lng')
+    print('usage: osm.py lat lng')
     exit(1)
 
 lat = sys.argv[1]
@@ -18,7 +21,7 @@ project = pyproj.Transformer.from_crs(wgs84, osgb36, always_xy=True).transform
 api = overpass.API()
 response = api.get("""way["building"](around:5, %s, %s)""" % (lat, lng), verbosity='geom')
 
-if len(response.features) != 1:
+if len(response.features) < 1:
     print('no building found')
     exit(1)
 
@@ -37,3 +40,6 @@ print(depth)
 
 width = min(edge_length)
 print(width)
+
+stats = zonal_stats(projected_poly, raster)
+print(stats[0].get("max"))
